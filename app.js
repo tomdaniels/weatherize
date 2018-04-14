a786eafd7c997415da71f182dc59bceefd87b041
tomdaniels <thomsondaniels@hotmail.com>
2018-04-14 19:04:14 +1000

    allow user to pass dynamic address to fetch api

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ const argv = yargs
   .alias('help', 'h') // two arguments. Actual command and new alias.
   .argv;
 
-  console.log(argv);
-
+  var userInput = argv.address;
+  var address = encodeURIComponent(userInput);
 // takes two arguments (options, callback)
 // https://www.npmjs.com/package/request
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBrbTVAFbFHxscReX_Bgt-LnvPh_vwPbDQ&address=100%20harris%20street%20pyrmont',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBrbTVAFbFHxscReX_Bgt-LnvPh_vwPbDQ&address=${address}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
