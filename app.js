1c1adba237fc095661134fe910308b888c0ccffe
tomdaniels <thomsondaniels@hotmail.com>
2018-04-14 18:12:39 +1000

    get geolocation for property

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ request({
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
