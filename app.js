6177a4b57eeb69b7d69ab140103055ebf644ed4e
tomdaniels <thomsondaniels@hotmail.com>
2018-04-14 17:59:29 +1000

    pretty printing objects in console

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBrbTVAFbFHxscReX_Bgt-LnvPh_vwPbDQ&address=100%20harris%20street%20pyrmont',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
