b99083e081d3c2a635aa1cedc2b37130f2926be1
tomdaniels <thomsondaniels@hotmail.com>
2018-04-14 19:26:28 +1000

    error handling

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBrbTVAFbFHxscReX_Bgt-LnvPh_vwPbDQ&address=${address}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log('Unable to connect to Google services');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address/location');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
