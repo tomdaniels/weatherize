ab51baf68744616b37cc2c75107e5029161f1f29
tomdaniels <thomsondaniels@hotmail.com>
2018-04-14 20:31:27 +1000

    refactor

--- utils/weather.js
+++ utils/weather.js
@@ -2,7 +2,8 @@ const request = require('request');
 
 function fetchWeather(latitude, longitude, address, callback) {
   request({
-    url: `https://api.darksky.net/forecast/f64538d78bcb20f01ffae5ccb6c7cc16/${latitude},${longitude}`,
+    url: `https://api.darksky.net/forecast/
+    f64538d78bcb20f01ffae5ccb6c7cc16/${latitude},${longitude}`,
     json: true
   }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
