1128c94f8e6ef2772a9bd4e75a14b4f873a15625
tomdaniels <thomsondaniels@hotmail.com>
2018-04-15 12:48:39 +1000

    expanding on promises

--- playground/promise.js
+++ playground/promise.js
@@ -1,23 +1,41 @@
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('hey it worked!');
-    // reject('this is an error')
-  }, 2500);
+var asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    }, 1500);
+  });
+};
+
+asyncAdd(5, 'b').then((result) => {
+  console.log(`result: ${result}`);
+}, (error) => {
+  console.log(`error: ${error}`)
 });
 
-// can only resolve/reject a promise once
-// once the state is set to resolve/rejected it can't be changed
-
-// somePromise.then(message => {
-//     console.log(`mesage: ${message}`);
-// })
-// .catch(error => console.log(`error: ${error}`))
-
-// You can provide two functions as arguments to the then prototype on promises.
-// instead of explicitly chaining .then().catch();
-
-somePromise.then((result) => {
-  console.log(`message: ${result}`)
-}, (errorMessage) => {
-  console.log(`error: ${errorMessage}`)
-});
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('hey it worked!');
+//     // reject('this is an error')
+//   }, 2500);
+// });
+//
+// // can only resolve/reject a promise once
+// // once the state is set to resolve/rejected it can't be changed
+//
+// // somePromise.then(message => {
+// //     console.log(`mesage: ${message}`);
+// // })
+// // .catch(error => console.log(`error: ${error}`))
+//
+// // You can provide two functions as arguments to the then prototype on promises.
+// // instead of explicitly chaining .then().catch();
+//
+// somePromise.then((result) => {
+//   console.log(`message: ${result}`)
+// }, (errorMessage) => {
+//   console.log(`error: ${errorMessage}`)
+// });
