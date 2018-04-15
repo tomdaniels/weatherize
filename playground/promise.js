b48f567c0dbabf9c38e3e4f87eda736d1c4a9064
tomdaniels <thomsondaniels@hotmail.com>
2018-04-15 12:43:06 +1000

    provide two callbacks to .then() instead of chaining .catch()

--- playground/promise.js
+++ playground/promise.js
@@ -1,14 +1,23 @@
 var somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve('hey it worked!');
-    reject('this is an error')
+    resolve('hey it worked!');
+    // reject('this is an error')
   }, 2500);
 });
 
 // can only resolve/reject a promise once
 // once the state is set to resolve/rejected it can't be changed
 
-somePromise.then(message => {
-    console.log(`mesage: ${message}`);
-})
-.catch(error => console.log(`error: ${error}`))
+// somePromise.then(message => {
+//     console.log(`mesage: ${message}`);
+// })
+// .catch(error => console.log(`error: ${error}`))
+
+// You can provide two functions as arguments to the then prototype on promises.
+// instead of explicitly chaining .then().catch();
+
+somePromise.then((result) => {
+  console.log(`message: ${result}`)
+}, (errorMessage) => {
+  console.log(`error: ${errorMessage}`)
+});
